80569b7a84a8cebc0eacee8161867db38e5665eb
ozbe <1372945+ozbe@users.noreply.github.com>
2020-05-24 13:36:51 -0400

    fix local_exec in install; correct install label for local_down;

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ echo "alias local_build=\"docker-compose -f $PWD/docker-compose.yml build --no-c
 # local_up - start local from anywhere
 echo "alias local_up=\"docker-compose -f $PWD/docker-compose.yml up -d\"" >> ~/.zshrc
 
-# local_up - start local session from anywhere
-echo "alias local_up=\"docker-compose -f $PWD/docker-compose.yml exec zsh\"" >> ~/.zshrc
+# local_exec - start local session from anywhere
+echo "alias local_exec=\"docker-compose -f $PWD/docker-compose.yml exec zsh\"" >> ~/.zshrc
 
-# local_up - sstop local from anywhere
+# local_down - stop local from anywhere
 echo "alias local_down=\"docker-compose -f $PWD/docker-compose.yml down\"" >> ~/.zshrc
 
 source ~/.zshrc
